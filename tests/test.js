31584dc94d2770fa83c2726448ced4efb6e2a849
Peter Muller <peterianmuller.com>
2016-12-02 12:29:15 -0800

    Updated a test with chai.should();

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,16 @@
 describe('Vasya - chack', function(){
+    var should = chai.should();
+
 	it('should return "YES" when the clerk can make change', function(){
 		expect(tickets([25, 25, 50, 50])).to.eql("YES");
 	});
+
+	it('should return "YES" when the clerk can make change', function(){
+		(tickets([25,25])).should.eql("YES");
+	});
+
+	it('should return "NO" when the clerk can\'t make change', function(){
+		expect(tickets([25, 25, 50, 20])).to.eql("NO");
+	});
+
 });
